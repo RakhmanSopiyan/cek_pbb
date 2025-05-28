4f53e5b0a5a8341538e4fffbcba7be20c4414b5c
RakhmanSopiyan <136042780+RakhmanSopiyan@users.noreply.github.com>
2025-05-28 15:16:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def index():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Gunakan PORT dari Railway
+    port = int(os.environ.get("PORT", 8080))  # Gunakan PORT dari Railway
     app.run(host="0.0.0.0", port=port)
